5103e6fb902e9dab5865b10e042d5736818be512
Fifame30 <fifame.gloria.valles.ayadji@etu.univ-st-etienne.fr>
2022-01-14 13:02:10 +0100

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,4 +17,35 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+      /**
+     * @Route("/cv", name="cv")
+     */
+    public function cv(): Response
+    {
+        return $this->render('home/cv.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+      /**
+     * @Route("/portfolio", name="portfolio")
+     */
+    public function portfolio(): Response
+    {
+        return $this->render('home/portfolio.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+      /**
+     * @Route("/about", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
 }
